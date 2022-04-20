0eb59f5959768d8eee24a328aedaee5bd6e46b4c
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2022-04-20 11:38:44 +0200

    test pub/sub

---
+++ pkg/message/publisher_test.go
@@ -0,0 +1,63 @@
+package message
+
+import (
+	"bytes"
+	"context"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/sirupsen/logrus"
+)
+
+func TestPublisher(t *testing.T) {
+	type testmsg struct {
+		Name string
+	}
+
+	topic := &mocktopic{}
+	buf := &bytes.Buffer{}
+	log := logrus.New()
+	log.Out = buf
+	log.Level = logrus.DebugLevel
+
+	p := Publisher[testmsg]{
+		topic: topic,
+		log:   log.WithTime(time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC)),
+	}
+
+	err := p.Publish(context.Background(), testmsg{Name: "hello"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	p.Stop()
+
+	if len(topic.messages) != 1 {
+		t.Fatalf("Expected 1 message, got %d", len(topic.messages))
+	}
+	if !bytes.Equal(topic.messages[0].Data, []byte(`{"Name":"hello"}`)) {
+		t.Errorf("Expected different message, got %v", string(topic.messages[0].Data))
+	}
+	if buf.String() != "time=\"2020-01-01T00:00:00Z\" level=debug msg=\"Published message\" topic=topic\n" {
+		t.Errorf("Expected different log, got %q", buf.String())
+	}
+	if !topic.stopped {
+		t.Error("Expected topic to be stopped")
+	}
+}
+
+type mocktopic struct {
+	messages []*pubsub.Message
+	stopped  bool
+}
+
+func (m *mocktopic) Publish(ctx context.Context, msg *pubsub.Message) *pubsub.PublishResult {
+	m.messages = append(m.messages, msg)
+	return &pubsub.PublishResult{}
+}
+
+func (m *mocktopic) String() string { return "topic" }
+
+func (m *mocktopic) Stop() {
+	m.stopped = true
+}
---
+++ pkg/message/subscriber_test.go
@@ -0,0 +1,64 @@
+package message
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+	"cloud.google.com/go/pubsub/pstest"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func TestSubscriber(t *testing.T) {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 4*time.Second)
+	defer cancel()
+
+	// Start a fake server running locally.
+	srv := pstest.NewServer()
+	defer srv.Close()
+	// Connect to the server without using TLS.
+	conn, err := grpc.Dial(srv.Addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+	// Use the connection when creating a pubsub client.
+	client, err := pubsub.NewClient(ctx, "project", option.WithGRPCConn(conn))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+	_ = client // TODO: Use the client.
+
+	topic, err := client.CreateTopic(ctx, "topic")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = client.CreateSubscription(ctx, "subscription", pubsub.SubscriptionConfig{Topic: client.Topic("topic")})
+	if err != nil {
+		t.Fatal(err)
+	}
+	topic.Publish(ctx, &pubsub.Message{Data: []byte(`{"Name":"test"}`)})
+
+	type testmsg struct{ Name string }
+	sub := NewSubscriber[testmsg](client, "project", "subscription")
+	sub.Synchronous()
+
+	ctx, done := context.WithCancel(ctx)
+	msgs := []testmsg{}
+	sub.Receive(ctx, func(ctx context.Context, msg testmsg) error {
+		msgs = append(msgs, msg)
+		done()
+		return nil
+	})
+
+	<-ctx.Done()
+
+	if len(msgs) != 1 {
+		t.Errorf("expected 1 message, got %d", len(msgs))
+	}
+}
