e245020c6bf262eaa74603085e84cccb15fbe1a5
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2022-08-05 14:13:35 +0200

    Add navigation heading for tenant environments

--- frontend/pages/tenant/[tenantName]/[[...environmentName]].tsx
+++ frontend/pages/tenant/[tenantName]/[[...environmentName]].tsx
@@ -7,7 +7,6 @@ import Environment from '../../../components/tenant/environment'
 import {Main, MenuItem, MenuItems, PageContainer, SideMenu} from '../../../components/lib/PageLayout'
 import BreadCrumb from "../../../components/lib/breadcrumb";
 
-
 const Tenant = () => {
   const router = useRouter()
   const tenantName = router.query.tenantName as string
@@ -27,6 +26,8 @@ const Tenant = () => {
     <PageContainer>
       <SideMenu>
         <MenuItems>
+          <span style={{marginBottom: "15px"}}>Environments</span>
+
           {tenant.environments?.map((e, i) => {
             return (
               <MenuItem onClick={() => router.push(`/tenant/${tenantName}/${e.name}`)} key={`${e.name}_${i}`} active={e.name == environmentName}>
