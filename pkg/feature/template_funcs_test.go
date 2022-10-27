f56238a3b98fcfbe4dc368c78446cd58f8a49c21
thokra-nav <85170275+thokra-nav@users.noreply.github.com>
2022-10-27 03:37:30 -0700

    Fn (#245)
    * mapping functions and test for mapping templates
    
    Co-authored-by: frodesundby <frode.sundby@nav.no>

---
+++ pkg/feature/template_funcs_test.go
@@ -0,0 +1,476 @@
+package feature
+
+import (
+	"bytes"
+	"testing"
+	"text/template"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/nais/fasit/pkg/graph/model"
+)
+
+func Test_mapOf(t *testing.T) {
+	type args struct {
+		m     []map[string]any
+		key   string
+		value string
+	}
+	tests := map[string]struct {
+		args args
+		want map[string]any
+	}{
+		"empty slice": {
+			args: args{
+				m:     []map[string]any{},
+				key:   "foo",
+				value: "bar",
+			},
+			want: map[string]any{},
+		},
+		"multiple slices": {
+			args: args{
+				m: []map[string]any{
+					{
+						"key": "bar",
+						"val": "qux",
+					},
+					{
+						"key": "baz",
+						"val": "quux",
+					},
+					{
+						"key": "ignored",
+					},
+				},
+				key:   "key",
+				value: "val",
+			},
+			want: map[string]any{
+				"bar": "qux",
+				"baz": "quux",
+			},
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			if got := mapOf(tt.args.m, tt.args.key, tt.args.value); !cmp.Equal(got, tt.want) {
+				t.Errorf("diff -want +got:\n%v", cmp.Diff(tt.want, got))
+			}
+		})
+	}
+}
+
+func Test_mapJoin(t *testing.T) {
+	type args struct {
+		m   any
+		sep string
+	}
+	tests := map[string]struct {
+		args args
+		want []string
+	}{
+		"empty map": {
+			args: args{
+				m:   map[string]any{},
+				sep: "=",
+			},
+			want: []string{},
+		},
+		"map with one entry": {
+			args: args{
+				m: map[string]any{
+					"foo": "bar",
+				},
+				sep: "=",
+			},
+			want: []string{"foo=bar"},
+		},
+		"map with multiple entries": {
+			args: args{
+				m: map[string]any{
+					"foo": "bar",
+					"baz": "qux",
+				},
+				sep: "=",
+			},
+			want: []string{"baz=qux", "foo=bar"},
+		},
+		"map with string map": {
+			args: args{
+				m: map[string]string{
+					"foo": "bar",
+					"baz": "qux",
+				},
+				sep: "=",
+			},
+			want: []string{"baz=qux", "foo=bar"},
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			if got := mapJoin(tt.args.sep, tt.args.m); !cmp.Equal(got, tt.want) {
+				t.Errorf("diff -want +got:\n%v", cmp.Diff(tt.want, got))
+			}
+		})
+	}
+}
+
+func Test_prefixedValues(t *testing.T) {
+	type args struct {
+		m      any
+		prefix string
+	}
+	tests := map[string]struct {
+		args args
+		want []any
+	}{
+		"empty map": {
+			args: args{
+				m:      map[string]any{},
+				prefix: "foo",
+			},
+			want: []any{},
+		},
+		"map with matching key": {
+			args: args{
+				m: map[string]any{
+					"foo": "bar",
+				},
+				prefix: "foo",
+			},
+			want: []any{"bar"},
+		},
+		"map with non-matching key": {
+			args: args{
+				m: map[string]any{
+					"foo": "bar",
+				},
+				prefix: "baz",
+			},
+			want: []any{},
+		},
+		"map with matching key and non-matching key": {
+			args: args{
+				m: map[string]any{
+					"foo": "bar",
+					"baz": "qux",
+				},
+				prefix: "foo",
+			},
+			want: []any{"bar"},
+		},
+		"map with prefixes": {
+			args: args{
+				m: map[string]any{
+					"foo":  "bar",
+					"fbaz": "qux",
+				},
+				prefix: "f",
+			},
+			want: []any{"bar", "qux"},
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			if got := prefixedValues(tt.args.m, tt.args.prefix); !cmp.Equal(got, tt.want) {
+				t.Errorf("diff -want +got:\n%v", cmp.Diff(tt.want, got))
+			}
+		})
+	}
+}
+
+func Test_subdomain(t *testing.T) {
+	type args struct {
+		m      *MappingValues
+		prefix string
+	}
+	tests := map[string]struct {
+		args args
+		want string
+	}{
+		"management": {
+			args: args{
+				m: &MappingValues{
+					Kind: model.EnvironmentKindManagement,
+					Tenant: MappingTenant{
+						Name: "tenant",
+					},
+				},
+				prefix: "foo",
+			},
+			want: "foo.tenant.cloud.nais.io",
+		},
+		"non-management": {
+			args: args{
+				m: &MappingValues{
+					Kind: model.EnvironmentKindTenant,
+					Env: map[string]any{
+						"name": "bar",
+					},
+					Tenant: MappingTenant{
+						Name: "baz",
+					},
+				},
+				prefix: "foo",
+			},
+			want: "foo.bar.baz.cloud.nais.io",
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			if got := subdomain(tt.args.m, tt.args.prefix); !cmp.Equal(got, tt.want) {
+				t.Errorf("diff -want +got:\n%v", cmp.Diff(tt.want, got))
+			}
+		})
+	}
+}
+
+func Test_eachOf(t *testing.T) {
+	type testStruct struct {
+		Foo string
+	}
+
+	type args struct {
+		m   any
+		key string
+	}
+	tests := map[string]struct {
+		args args
+		want []any
+	}{
+		"empty slice": {
+			args: args{
+				m:   []map[string]any{},
+				key: "foo",
+			},
+			want: []any{},
+		},
+		"slice of map with matching key": {
+			args: args{
+				m: []map[string]any{
+					{
+						"foo": "bar",
+					},
+				},
+				key: "foo",
+			},
+			want: []any{"bar"},
+		},
+		"slice of map with non-matching key": {
+			args: args{
+				m: []map[string]any{
+					{
+						"foo": "bar",
+					},
+				},
+				key: "baz",
+			},
+			want: []any{},
+		},
+
+		"slice of map with matching key and non-matching key": {
+			args: args{
+				m: []map[string]any{
+					{
+						"foo": "bar",
+					},
+					{
+						"baz": "qux",
+					},
+				},
+				key: "foo",
+			},
+			want: []any{"bar"},
+		},
+		"slice of map with matching key and non-matching key and matching key": {
+			args: args{
+				m: []map[string]any{
+					{
+						"foo": "bar",
+					},
+					{
+						"baz": "qux",
+					},
+					{
+						"foo": "quux",
+					},
+				},
+				key: "foo",
+			},
+			want: []any{"bar", "quux"},
+		},
+
+		"slice of struct with matching key": {
+			args: args{
+				m: []testStruct{
+					{
+						Foo: "bar",
+					},
+				},
+				key: "Foo",
+			},
+			want: []any{"bar"},
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			if got := eachOf(tt.args.m, tt.args.key); !cmp.Equal(got, tt.want) {
+				t.Errorf("diff -want +got:\n%v", cmp.Diff(tt.want, got))
+			}
+		})
+	}
+}
+
+func Test_toJSON(t *testing.T) {
+	tests := map[string]struct {
+		arg  any
+		want string
+	}{
+		"empty map": {
+			arg:  map[string]any{},
+			want: "{}",
+		},
+		"map with one key": {
+			arg: map[string]any{
+				"foo": "bar",
+			},
+			want: `{"foo":"bar"}`,
+		},
+		"slice with one key": {
+			arg: []string{
+				"foo",
+			},
+			want: `["foo"]`,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			if got := toJSON(tt.arg); !cmp.Equal(got, tt.want) {
+				t.Errorf("diff -want +got:\n%v", cmp.Diff(tt.want, got))
+			}
+		})
+	}
+}
+
+func Test_toYAML(t *testing.T) {
+	tests := map[string]struct {
+		arg  any
+		want string
+	}{
+		"empty map": {
+			arg:  map[string]any{},
+			want: "{}\n",
+		},
+		"map with one key": {
+			arg: map[string]any{
+				"foo": "bar",
+			},
+			want: "foo: bar\n",
+		},
+		"slice with one key": {
+			arg: []string{
+				"foo",
+			},
+			want: "- foo\n",
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			if got := toYAML(tt.arg); !cmp.Equal(got, tt.want) {
+				t.Errorf("diff -want +got:\n%v", cmp.Diff(tt.want, got))
+			}
+		})
+	}
+}
+
+func Test_usage(t *testing.T) {
+	tests := map[string]struct {
+		template string
+		values   *MappingValues
+		want     string
+	}{
+		"eachOf piped to toJSON": {
+			template: `{{ eachOf .Envs "foo" | toJSON }}`,
+			values: &MappingValues{
+				Envs: []map[string]any{
+					{
+						"foo": "bar",
+					},
+					{
+						"foo": "baz",
+					},
+				},
+			},
+			want: `["bar","baz"]`,
+		},
+		"mapOf piped to mapJoin piped to join": {
+			template: `{{ mapOf .Envs "name" "project_id" | mapJoin "=" | join "," }}`,
+			values: &MappingValues{
+				Envs: []map[string]any{
+					{
+						"name":       "foo",
+						"project_id": "bar",
+					},
+					{
+						"name":       "baz",
+						"project_id": "qux",
+					},
+				},
+			},
+			want: `baz=qux,foo=bar`,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			tpl := template.New("usage")
+			tpl.Funcs(templateFuncs)
+			tpl, err := tpl.Parse(tt.template)
+			if err != nil {
+				t.Fatalf("failed to parse template: %v", err)
+			}
+
+			var got bytes.Buffer
+			if err := tpl.Execute(&got, tt.values); err != nil {
+				t.Fatalf("failed to execute template: %v", err)
+			}
+			if !cmp.Equal(tt.want, got.String()) {
+				t.Errorf("diff -want +got:\n%v", cmp.Diff(tt.want, got.String()))
+			}
+		})
+	}
+}
+
+func Test_join(t *testing.T) {
+	tests := map[string]struct {
+		list []string
+		sep  string
+		want string
+	}{
+		"empty slice": {
+			list: []string{},
+			sep:  ",",
+			want: "",
+		},
+		"slice with one element": {
+			list: []string{"foo"},
+			sep:  ",",
+			want: "foo",
+		},
+		"slice with two elements": {
+			list: []string{"foo", "bar"},
+			sep:  ",",
+			want: "foo,bar",
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			if got := join(tt.sep, tt.list); !cmp.Equal(got, tt.want) {
+				t.Errorf("diff -want +got:\n%v", cmp.Diff(tt.want, got))
+			}
+		})
+	}
+}
