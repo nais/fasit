761b903c5631b3b3098a569decd0ed219725be9b
Johnny Horvi <johnny@horvi.no>
2023-01-09 21:25:01 +0100

    add bump script

---
+++ hack/bump.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+feature=$1
+version=$2
+
+if [ -z "$feature" ] || [ -z "$version" ]; then
+    echo "Usage: $0 <feature> <version>"
+    exit 1
+fi
+
+if [ ! -f features/$feature.yaml ]; then
+    echo "Feature $feature does not exist"
+    exit 1
+fi
+
+sed -i "s/^version:.*/version: $version/" features/$feature.yaml
+
+git add features/$feature.yaml
+git commit -m "Bump $feature to $version"
+git push
+
