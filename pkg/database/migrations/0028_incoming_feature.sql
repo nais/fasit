96299e634114a2b86017f0ab8fa682349eac5501
Johnny Horvi <johnny@horvi.no>
2023-02-15 14:00:36 +0100

    wip: new tables

--- pkg/database/migrations/0028_incoming_feature.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-CREATE TABLE incoming_feature(
-    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name text NOT NULL UNIQUE,
-    version text NOT NULL,
-    created_at timestamp with time zone NOT NULL DEFAULT now()
-);
