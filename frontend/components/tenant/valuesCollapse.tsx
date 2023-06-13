4c84bebe61d9a0bffc3ce73e6de292ad8f1d1f43
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2023-06-13 13:43:54 +0200

    Final react-tooltip?

--- frontend/components/tenant/valuesCollapse.tsx
+++ frontend/components/tenant/valuesCollapse.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import useCollapse from 'react-collapsed';
-import styled from "styled-components";
-import {Expand, Collapse} from "@navikt/ds-icons";
-import {MappingValue} from "../../lib/schema/graphql";
-import ReactTooltip from "react-tooltip";
+import React from 'react'
+import useCollapse from 'react-collapsed'
+import styled from 'styled-components'
+import { Expand, Collapse } from '@navikt/ds-icons'
+import { MappingValue } from '../../lib/schema/graphql'
+import { Tooltip } from 'react-tooltip'
 
 const LogPre = styled.pre`
   word-break: break-word;
@@ -12,61 +12,68 @@ const LogPre = styled.pre`
 `
 
 const Header = styled.div`
-    cursor: pointer;
-    text-align: left;
+  cursor: pointer;
+  text-align: left;
 `
 
 interface DropdownProps {
-    content: MappingValue
+  content: MappingValue
 }
 
 const isJson = (x: any) => {
-    try {
-        JSON.parse(x);
-    } catch (e) {
-        return false;
-    }
-    return true;
+  try {
+    JSON.parse(x)
+  } catch (e) {
+    return false
+  }
+  return true
 }
 
 const isObject = (x: any) => {
-    return x.toString() === '[object Object]'
+  return x.toString() === '[object Object]'
 }
 
 function safeToString(x: any) {
-    if (typeof x === "string") {
-        return x
-    }
+  if (typeof x === 'string') {
+    return x
+  }
 
-    if (isJson(x) || isObject(x)) {
-        return JSON.stringify(x, null, 2)
-    }
+  if (isJson(x) || isObject(x)) {
+    return JSON.stringify(x, null, 2)
+  }
 
-    return x.toString()
+  return x.toString()
 }
 
-const ValuesCollapse = ({content}: DropdownProps) => {
-    const {getCollapseProps, getToggleProps, isExpanded} = useCollapse();
-    return (
-        Array.isArray(content.value) && content.value.length > 1 ?
-            <div className="collapsible">
-                <Header {...getToggleProps()}>
-                    {isExpanded ?
-                        <>
-                            <Collapse data-tip data-for={"collapse"}/>
-                            <ReactTooltip id='collapse' place='top' type='dark' effect='solid'> Collapse </ReactTooltip>
-                        </> :
-                        <>
-                            <Expand data-tip data-for={"expand"}/>
-                            <ReactTooltip id='expand' place='top' type='dark' effect='solid'> Expand </ReactTooltip>
-                        </>
-                    }
-                </Header>
-                <div {...getCollapseProps()}>
-                    <LogPre>{JSON.stringify(content.value, null, 2)}</LogPre>
-                </div>
-            </div>
-            : safeToString(content.value)
-    );
+const ValuesCollapse = ({ content }: DropdownProps) => {
+  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse()
+  return Array.isArray(content.value) && content.value.length > 1 ? (
+    <div className="collapsible">
+      <Header {...getToggleProps()}>
+        {isExpanded ? (
+          <>
+            <Collapse data-tip data-for={'collapse'} />
+            <Tooltip id="collapse" place="top" variant="dark">
+              {' '}
+              Collapse{' '}
+            </Tooltip>
+          </>
+        ) : (
+          <>
+            <Expand data-tip data-for={'expand'} />
+            <Tooltip id="expand" place="top" variant="dark">
+              {' '}
+              Expand{' '}
+            </Tooltip>
+          </>
+        )}
+      </Header>
+      <div {...getCollapseProps()}>
+        <LogPre>{JSON.stringify(content.value, null, 2)}</LogPre>
+      </div>
+    </div>
+  ) : (
+    safeToString(content.value)
+  )
 }
 export default ValuesCollapse
