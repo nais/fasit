abbc446627352707df48b8e6b3fa9aa6ec19b398
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2023-08-29 10:55:14 +0200

    Manually add proxy

--- pkg/helm/pull.go
+++ pkg/helm/pull.go
@@ -3,8 +3,11 @@ package helm
 import (
 	"bytes"
 	"fmt"
+	"net"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 
 	"helm.sh/helm/v3/pkg/cli"
 	"helm.sh/helm/v3/pkg/downloader"
@@ -46,7 +49,29 @@ func DownloadChart(chart, version, repo string) (*bytes.Buffer, error) {
 		return nil, fmt.Errorf("unable to get getter for scheme %s: %w", u.Scheme, err)
 	}
 
-	b, err := get.Get(u.String())
+	opts := []getter.Option{}
+	if _, ok := os.LookupEnv("HTTPS_PROXY"); ok {
+		opts = append(opts, getter.WithTransport(
+			&http.Transport{
+				// From https://github.com/google/go-containerregistry/blob/31786c6cbb82d6ec4fb8eb79cd9387905130534e/pkg/v1/remote/options.go#L87
+				DisableCompression: true,
+				Proxy:              http.ProxyFromEnvironment,
+				DialContext: (&net.Dialer{
+					// By default we wrap the transport in retries, so reduce the
+					// default dial timeout to 5s to avoid 5x 30s of connection
+					// timeouts when doing the "ping" on certain http registries.
+					Timeout:   5 * time.Second,
+					KeepAlive: 30 * time.Second,
+				}).DialContext,
+				ForceAttemptHTTP2:     true,
+				MaxIdleConns:          100,
+				IdleConnTimeout:       90 * time.Second,
+				TLSHandshakeTimeout:   10 * time.Second,
+				ExpectContinueTimeout: 1 * time.Second,
+			}))
+	}
+
+	b, err := get.Get(u.String(), opts...)
 	if err != nil {
 		return nil, fmt.Errorf("unable to get chart: %w", err)
 	}
