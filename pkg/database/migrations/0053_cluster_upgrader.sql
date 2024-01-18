ee56e8c0a93dad05e7f373c8447a4d24e2b942c3
Roger Bjørnstad <roger.bjornstad@nav.no>
2024-01-18 16:22:42 +0100

    Add cluster version and cluster upgrader tables and queries
    Add cluster version and cluster upgrader tables and queries to support environment upgrades.
    
    Co-authored-by: ybelMekk <youssef.bel.mekki@nav.no>

---
+++ pkg/database/migrations/0053_cluster_upgrader.sql
@@ -0,0 +1,31 @@
+-- +goose Up
+CREATE TABLE cluster_upgrade (
+    "operation_id" TEXT PRIMARY KEY,
+    "tenant_id" UUID NOT NULL,
+    "environment_id" UUID NOT NULL,
+    "version_id" UUID NOT NULL,
+    "status" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "nodes_total" INT NOT NULL,
+    "nodes_failed" INT NOT NULL,
+    "nodes_completed" INT NOT NULL,
+    "nodes_done" INT NOT NULL,
+    "node_pdb_delay_seconds" INT NOT NULL,
+    "start_time" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "last_modified" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    CONSTRAINT fk_cluster_upgrade_tenant
+      FOREIGN KEY (tenant_id)
+          REFERENCES tenants (id) ON DELETE CASCADE,
+    CONSTRAINT fk_cluster_upgrade_env
+      FOREIGN KEY (environment_id)
+          REFERENCES environments (id) ON DELETE CASCADE,
+    CONSTRAINT fk_cluster_upgrade_version
+      FOREIGN KEY (version_id)
+          REFERENCES cluster_version (id) ON DELETE CASCADE
+);
+
+CREATE TRIGGER cluster_upgrade_set_modified
+    BEFORE UPDATE
+    ON cluster_upgrade
+    FOR EACH ROW
+    EXECUTE PROCEDURE update_modified_timestamp();
--- pkg/database/queries/cluster_upgrade.sql
+++ pkg/database/queries/cluster_upgrade.sql
@@ -1,15 +1,18 @@
 -- name: ClusterOperationCreateOrUpdate :one
 INSERT INTO cluster_upgrade
-("operation_id", "tenant_id", "environment_id", "status", "type", "master_version", "nodes_total", "nodes_failed","nodes_completed", "nodes_done", "node_pdb_delay_seconds")
+("operation_id", "tenant_id", "environment_id", "status", "type", "nodes_total", "nodes_failed","nodes_completed", "nodes_done", "node_pdb_delay_seconds")
 VALUES
-(@operationId, @tenantId, @envID, @status, @type, @masterVersion, @nodesTotal, @nodesFailed, @nodesCompleted, @nodesDone, @nodePdbDelaySeconds)
+(@operationId, @tenantId, @envID, @status, @type, @nodesTotal, @nodesFailed, @nodesCompleted, @nodesDone, @nodePdbDelaySeconds)
 ON CONFLICT ("operation_id", "tenant_id", "environment_id") DO
 UPDATE SET
     "status" = EXCLUDED.status,
-    "master_version" = EXCLUDED.master_version,
     "nodes_total" = EXCLUDED.nodes_total,
     "nodes_failed" = EXCLUDED.nodes_failed,
     "nodes_completed" = EXCLUDED.nodes_completed,
     "nodes_done" = EXCLUDED.nodes_done,
     "node_pdb_delay_seconds" = EXCLUDED.node_pdb_delay_seconds
 RETURNING *;
+
+-- name: ClusterOperationsGet :many
+SELECT * FROM cluster_upgrade WHERE "tenant_id" = @tenantId AND "environment_id" = @envID AND "status" = @status
+ORDER BY "start_time" DESC;
