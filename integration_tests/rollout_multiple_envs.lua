a6d2926c0b1f641b98444a750951a9c3c521a320
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-02-18 17:15:29 +0100

    Add rollout then failed

--- integration_tests/rollout_multiple_envs.lua
+++ integration_tests/rollout_multiple_envs.lua
@@ -1,7 +1,7 @@
 local tenantID = Helper.CreateTenant("tenant23", true)
 local managementID = Helper.CreateEnvironment(tenantID, "management", "management", false, true)
 local testingID = Helper.CreateEnvironment(tenantID, "testing", "tenant", false, true)
-local nonciID = Helper.CreateEnvironment(tenantID, "nonci", "tenant")
+Helper.CreateEnvironment(tenantID, "nonci", "tenant")
 
 Helper.SQLExec("UPDATE environments SET reconcile = false WHERE id = $1", managementID)
 
