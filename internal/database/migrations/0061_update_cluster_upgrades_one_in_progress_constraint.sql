63ed30bd3e614e8ea48e5cec8a61faa516875ac9
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-10-31 15:50:22 +0100

    feat(migrations): add unique index for cluster upgrades with updated status conditions

---
+++ internal/database/migrations/0061_update_cluster_upgrades_one_in_progress_constraint.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+DROP INDEX IF EXISTS cluster_upgrades_one_in_progress
+;
+
+CREATE UNIQUE INDEX cluster_upgrades_one_in_progress ON cluster_upgrades (environment_id)
+WHERE
+	status IN (
+		'CREATED',
+		'WAITING',
+		'CONTROL_PLANE_UPGRADE',
+		'NODE_UPGRADE'
+	)
+;
+
+-- +goose Down
+ALTER TABLE cluster_upgrades
+DROP CONSTRAINT IF EXISTS cluster_upgrades_one_in_progress
+;
+
+CREATE UNIQUE INDEX cluster_upgrades_one_in_progress ON cluster_upgrades (environment_id)
+WHERE
+	status IN ('CREATED', 'MASTER_UPGRADE', 'NODE_UPGRADE')
+;
