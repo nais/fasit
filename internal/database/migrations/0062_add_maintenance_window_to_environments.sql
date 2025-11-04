a2558c7b582e9ae1b7016ccdfb313bdc72f6eaf9
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-11-04 10:22:30 +0100

    feat: add maintenance window column with validation to environments table

--- internal/database/migrations/0062_add_maintenance_window_to_environments.sql
+++ internal/database/migrations/0062_add_maintenance_window_to_environments.sql
@@ -3,13 +3,23 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE environments
-ADD COLUMN maintenance_window JSONB
+ADD COLUMN maintenance_window JSONB,
+ADD CONSTRAINT environments_maintenance_window_valid CHECK (
+	maintenance_window IS NULL
+	OR (
+		JSONB_TYPEOF(maintenance_window) = 'object'
+		AND maintenance_window ? 'day'
+		AND maintenance_window ? 'start_time'
+		AND maintenance_window ? 'end_time'
+	)
+)
 ;
 
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE environments
+DROP CONSTRAINT IF EXISTS environments_maintenance_window_valid,
 DROP COLUMN maintenance_window
 ;
 
