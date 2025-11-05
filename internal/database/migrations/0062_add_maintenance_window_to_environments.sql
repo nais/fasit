3530d5f744e78ade6b922c2df4851d9b67fbdde1
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-11-05 13:29:16 +0100

    feat: add validation for maintenance_window JSON structure in environments table

--- internal/database/migrations/0062_add_maintenance_window_to_environments.sql
+++ internal/database/migrations/0062_add_maintenance_window_to_environments.sql
@@ -9,9 +9,15 @@ ADD CONSTRAINT environments_maintenance_window_valid CHECK (
 	OR (
 		JSONB_TYPEOF(maintenance_window) = 'object'
 		AND JSONB_EXISTS (maintenance_window, 'startTime')
+		AND JSONB_TYPEOF(maintenance_window -> 'startTime') = 'string'
+		AND (maintenance_window ->> 'startTime') ~ '^\d{2}:\d{2}$'
 		AND JSONB_EXISTS (maintenance_window, 'endTime')
+		AND JSONB_TYPEOF(maintenance_window -> 'endTime') = 'string'
+		AND (maintenance_window ->> 'endTime') ~ '^\d{2}:\d{2}$'
 		AND JSONB_EXISTS (maintenance_window, 'days')
+		AND JSONB_TYPEOF(maintenance_window -> 'days') = 'array'
 		AND JSONB_EXISTS (maintenance_window, 'timezone')
+		AND JSONB_TYPEOF(maintenance_window -> 'timezone') = 'string'
 	)
 )
 ;
