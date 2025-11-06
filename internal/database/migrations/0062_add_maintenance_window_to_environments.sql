85a161864fd3f669ede6d9a1e05a47306d06abff
Roger Bjørnstad <roger.bjornstad@nav.no>
2025-11-06 16:04:11 +0100

    refactor: enhance validation regex for maintenance_window JSONB structure

--- internal/database/migrations/0062_add_maintenance_window_to_environments.sql
+++ internal/database/migrations/0062_add_maintenance_window_to_environments.sql
@@ -10,14 +10,15 @@ ADD CONSTRAINT environments_maintenance_window_valid CHECK (
 		JSONB_TYPEOF(maintenance_window) = 'object'
 		AND JSONB_EXISTS (maintenance_window, 'startTime')
 		AND JSONB_TYPEOF(maintenance_window -> 'startTime') = 'string'
-		AND (maintenance_window ->> 'startTime') ~ '^\d{2}:\d{2}$'
+		AND (maintenance_window ->> 'startTime') ~ '^([01]\d|2[0-3]):[0-5]\d$'
 		AND JSONB_EXISTS (maintenance_window, 'endTime')
 		AND JSONB_TYPEOF(maintenance_window -> 'endTime') = 'string'
-		AND (maintenance_window ->> 'endTime') ~ '^\d{2}:\d{2}$'
+		AND (maintenance_window ->> 'endTime') ~ '^([01]\d|2[0-3]):[0-5]\d$'
 		AND JSONB_EXISTS (maintenance_window, 'days')
 		AND JSONB_TYPEOF(maintenance_window -> 'days') = 'array'
 		AND JSONB_EXISTS (maintenance_window, 'timezone')
 		AND JSONB_TYPEOF(maintenance_window -> 'timezone') = 'string'
+		AND (maintenance_window ->> 'timezone') ~ '^[A-Za-z]+(?:/[A-Za-z_\-\.]+)+$'
 	)
 )
 ;
