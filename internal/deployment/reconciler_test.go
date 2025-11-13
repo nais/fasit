d28cdced83e3707890115c0393204f942c3b80d7
Tommy Trøen <tommy.troen@nav.no>
2025-11-13 14:04:55 +0100

    refactor: fix test, still fails
    Co-authored-by: christeredvartsen <christer.edvartsen@nav.no>

--- internal/deployment/reconciler_test.go
+++ internal/deployment/reconciler_test.go
@@ -3,15 +3,14 @@ package deployment_test
 import (
 	"context"
 	"fmt"
-	"io"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/nais/fasit/internal/database"
-	"github.com/nais/fasit/internal/database/dbtest"
 	"github.com/nais/fasit/internal/deployment"
 	"github.com/nais/fasit/internal/environment"
 	"github.com/nais/fasit/internal/feature"
@@ -21,6 +20,8 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
 )
 
 // Intentional uppercase to avoid var clashes
@@ -30,155 +31,37 @@ type Db struct {
 	pool *pgxpool.Pool
 }
 
-func (d Db) RunQuery(ctx context.Context, q string) (pgx.Rows, error) {
-	return d.pool.Query(ctx, q)
-}
-
-type featureDeploy struct {
-	name    string
-	version string
-	target  environment.Labels
-}
-
 type featureInput struct {
 	name, version string
 	dependencies  []string
-	labels        map[string]string
+	target        environment.Labels
 }
-
-func TestMultipleReconcile(t *testing.T) {
-	ctx := context.Background()
-	db := setupDb(ctx, t, true)
-
-	r, err := deployment.NewReconciler(
-		db.repo,
-		func(topicID string, log logrus.FieldLogger) deployment.Publisher {
-			return nil
-		},
-		nil,
-		nil,
-		logrus.NewEntry(logrus.New()),
-	)
-	if err != nil {
-		t.Fatalf("create reconciler: %v", err)
-	}
-
-	features := []featureInput{
-		{
-			name:    "monitoring",
-			version: "v1",
-			dependencies: []string{
-				"monitoring-crds",
-			},
-		},
-		{
-			name:    "monitoring-crds",
-			version: "v1",
-			// TODO: how to deploy a feature everywhere
-		},
-	}
-
-	envsToCreate := map[string]map[string]environment.Labels{
-		"nav": {
-			"dev": environment.Labels{
-				"aiven": "enabled",
-				"kind":  "tenant",
-			},
-			"management": environment.Labels{
-				"kind": "management",
-			},
-		},
-	}
-
-	db.createTenantEnvironments(ctx, envsToCreate)
-	db.createFeatureDeployments(ctx, features)
-
-	targetsAfterReconcile := [][]string{
-		{
-			"nav:dev:monitoring-crds:v1",
-			"nav:management:monitoring-crds:v1",
-		},
-		{
-			"nav:dev:monitoring-crds:v1",
-			"nav:management:monitoring-crds:v1",
-			"nav:dev:monitoring:v1",
-			"nav:managment:monitoring:v1",
-		},
-	}
-
-	for _, targets := range targetsAfterReconcile {
-		err = r.Reconcile(ctx)
-		if err != nil {
-			t.Fatalf("reconcile: %v", err)
-		}
-
-		allDeploymentTargets, err := db.repo.DeploymentTargetsGetAll(ctx)
-		if err != nil {
-			t.Fatalf("get all deployment targets: %v", err)
-		}
-		assert.Len(t, allDeploymentTargets, len(targets))
-		for _, dt := range allDeploymentTargets {
-			assert.Contains(t, targets, fmt.Sprintf("%s:%s:%s:%s", dt.TenantName, dt.EnvironmentName, dt.FeatureName, dt.Version))
-		}
-	}
-	fmt.Printf("%d features created\n", len(features))
-}
-
-type publisher struct {
-	log logrus.FieldLogger
-}
-
-func (p *publisher) Publish(_ context.Context, msg message.DeployInstruction) error {
-	p.log.Infof("%s:%s", msg.Name, msg.Version)
-	return nil
-}
-func (p *publisher) Stop() {}
 
 func TestReconcile(t *testing.T) {
 	ctx := context.Background()
-	db := setupDb(ctx, t, true)
 
-	logger, hook := test.NewNullLogger()
-
-	r, err := deployment.NewReconciler(
-		db.repo,
-		func(topicID string, log logrus.FieldLogger) deployment.Publisher {
-			return &publisher{log: log}
-		},
-		nil,
-		nil,
-		logger,
-	)
+	container, dsn, err := startPostgresql(ctx, t)
 	if err != nil {
-		t.Fatalf("create reconciler: %v", err)
+		t.Fatalf("failed to start postgres container: %v", err)
 	}
 
-	type tenantKey = string
-	envsToCreate := map[tenantKey]map[string]environment.Labels{
-		"test-partner": {
-			"dev": environment.Labels{},
-			"prod": environment.Labels{
-				"featuretoggle": "enabled",
-			},
-		},
-		"nav": {
-			"dev": environment.Labels{
-				"aiven": "enabled",
-			},
-			"management": environment.Labels{
-				"kind": "management",
-			},
-		},
+	logger, _ := test.NewNullLogger()
+
+	envsToCreate := map[string]environment.Labels{
+		"test-partner:dev":  {},
+		"test-partner:prod": {"featuretoggle": "enabled"},
+		"nav:dev":           {"aiven": "enabled"},
+		"nav:management":    {"kind": "management"},
 	}
 
 	tt := []struct {
 		name                string
-		deploymentsToCreate []featureDeploy
-		expectedFeatures    []string
+		deploymentsToCreate []featureInput
+		reconcileResults    [][]string
 	}{
 		{
 			name: "install most specific and latest features",
-			deploymentsToCreate: []featureDeploy{
+			deploymentsToCreate: []featureInput{
 				{name: "aivenator", version: "1.0.0", target: environment.Labels{"aiven": "enabled"}},
 				{name: "aivenator", version: "2.0.0", target: environment.Labels{"aiven": "enabled"}},
 				{name: "aivenator", version: "1.1.0", target: environment.Labels{"aiven": "enabled", "tenant": "nav"}},
@@ -189,27 +72,75 @@ func TestReconcile(t *testing.T) {
 				{name: "unleash", version: "2.0.0", target: environment.Labels{"kind": "tenant"}},
 				{name: "v13s", version: "1.0.0", target: environment.Labels{"kind": "management"}},
 			},
-			expectedFeatures: []string{
-				"nav:dev:aivenator:1.1.1",
-				"nav:dev:naiserator:1.0.0",
-				"nav:dev:unleash:2.0.0",
-				"nav:management:v13s:1.0.0",
-				"test-partner:dev:unleash:2.0.0",
-				"test-partner:prod:unleash:2.0.0",
+			reconcileResults: [][]string{
+				{
+					"nav:dev:aivenator:1.1.1",
+					"nav:dev:naiserator:1.0.0",
+					"nav:dev:unleash:2.0.0",
+					"nav:management:v13s:1.0.0",
+					"test-partner:dev:unleash:2.0.0",
+					"test-partner:prod:unleash:2.0.0",
+				},
+			},
+		},
+		{
+			name: "install features with dependencies",
+			deploymentsToCreate: []featureInput{
+				{
+					name:    "monitoring",
+					version: "v1",
+					dependencies: []string{
+						"monitoring-crds",
+					},
+					target: environment.Labels{"tenant": "nav"},
+				},
+				{
+					name:    "monitoring-crds",
+					version: "v1",
+					target:  environment.Labels{"tenant": "nav"},
+				},
+			},
+			reconcileResults: [][]string{
+				{
+					"nav:dev:monitoring-crds:v1",
+					"nav:management:monitoring-crds:v1",
+				},
+				{
+					"nav:dev:monitoring-crds:v1",
+					"nav:management:monitoring-crds:v1",
+					"nav:dev:monitoring:v1",
+					"nav:managment:monitoring:v1",
+				},
 			},
 		},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			db.createTenantEnvironments(ctx, envsToCreate)
-			db.createDeployments(ctx, tc.deploymentsToCreate)
-
-			if err := r.Reconcile(ctx); err != nil {
-				t.Fatalf("reconcile: %v", err)
+			fmt.Printf("dsn: %s\n", dsn)
+			db := getConnection(ctx, t, container, dsn, logger)
+			pub := &publisher{db: db}
+			r, err := deployment.NewReconciler(
+				db.repo,
+				func(topicID string, log logrus.FieldLogger) deployment.Publisher {
+					return pub
+				},
+				nil,
+				nil,
+				logger,
+			)
+			if err != nil {
+				t.Fatalf("create reconciler: %v", err)
 			}
 
-			q := `
+			db.createTenantEnvironments(ctx, envsToCreate)
+			db.createFeatureDeployments(ctx, tc.deploymentsToCreate)
+
+			for _, result := range tc.reconcileResults {
+				if err = r.Reconcile(ctx); err != nil {
+					t.Fatalf("reconcile: %v", err)
+				}
+				q := `
 				SELECT
 				t.name || ':' || e.name || ':' || di.feature_name || ':' || di.feature_version
 				FROM deploy_instructions di
@@ -217,60 +148,54 @@ func TestReconcile(t *testing.T) {
 				JOIN tenants t ON t.id = e.tenant_id
 			`
 
-			rows, err := db.RunQuery(ctx, q)
-			if err != nil {
-				t.Fatalf("query deploy instructions: %v", err)
-			}
-
-			var instructions []string
-			for rows.Next() {
-				var instruction string
-				_ = rows.Scan(&instruction)
-				instructions = append(instructions, instruction)
-			}
-
-			assert.Len(t, instructions, len(tc.expectedFeatures))
-			assert.Len(t, hook.Entries, len(tc.expectedFeatures))
-
-			for _, instruction := range tc.expectedFeatures {
-				found := false
-				for _, msg := range hook.Entries {
-					parts := strings.Split(instruction, ":")
-					if msg.Message == strings.Join(parts[2:], ":") {
-						found = true
-						break
-					}
+				rows, err := db.runQuery(ctx, q)
+				if err != nil {
+					t.Fatalf("query deploy instructions: %v", err)
 				}
 
-				if !found {
-					t.Errorf("expected log message for instruction %q not found", instruction)
+				var instructions []string
+				for rows.Next() {
+					var instruction string
+					_ = rows.Scan(&instruction)
+					instructions = append(instructions, instruction)
+				}
+
+				assert.Len(t, instructions, len(result))
+				assert.Len(t, pub.msg, len(result))
+
+				for _, instruction := range result {
+					found := false
+					for _, msg := range pub.msg {
+						parts := strings.Split(instruction, ":")
+						if fmt.Sprintf("%s:%s", msg.Name, msg.Version) == strings.Join(parts[2:], ":") {
+							found = true
+							break
+						}
+					}
+
+					if !found {
+						t.Errorf("expected log message for instruction %q not found", instruction)
+					}
 				}
 			}
 		})
 	}
 }
 
-func (d *Db) createDeployment(ctx context.Context, featureName, version string, labels environment.Labels) {
-	d.t.Helper()
-	err := d.repo.FeatureDataCreate(ctx, model.Feature{
-		FeatureYAML: model.FeatureYAML{},
-		Name:        featureName,
-		Version:     version,
-		Chart:       "oci://" + featureName,
-	}, &feature.FeatureTemplateDetails{})
-	if err != nil && !strings.Contains(err.Error(), "SQLSTATE 23505") {
-		d.t.Fatalf("create feature data: %v", err)
-	}
+type publisher struct {
+	msg []message.DeployInstruction
+	db  Db
+}
 
-	err = d.repo.FeatureVersionUpdate(ctx, featureName, version)
-	if err != nil {
-		d.t.Fatalf("update feature version: %v", err)
-	}
+func (p *publisher) Publish(ctx context.Context, msg message.DeployInstruction) error {
+	p.msg = append(p.msg, msg)
+	return p.db.repo.DeployInstructionUpdateStatus(ctx, msg.ID, model.RolloutStatusDeployed)
+}
 
-	_, err = d.repo.DeploymentCreate(ctx, featureName, version, []byte(`{"key": "ghref"}`), labels)
-	if err != nil {
-		d.t.Fatalf("create deployment: %v", err)
-	}
+func (p *publisher) Stop() {}
+
+func (d Db) runQuery(ctx context.Context, q string) (pgx.Rows, error) {
+	return d.pool.Query(ctx, q)
 }
 
 func (d *Db) createFeatureDeployments(ctx context.Context, features []featureInput) {
@@ -283,7 +208,7 @@ func (d *Db) createFeatureDeployments(ctx context.Context, features []featureInp
 				},
 			}
 		}
-		d.createFeatureDeployment(ctx, f.name, f.version, f.labels, deps)
+		d.createFeatureDeployment(ctx, f.name, f.version, f.target, deps)
 	}
 }
 
@@ -355,64 +280,66 @@ func (d *Db) createEnv(ctx context.Context, tenant *model.Tenant, name string, l
 }
 
 // key in envsToCreate is tenantName, key in inner map is env name
-func (d *Db) createTenantEnvironments(ctx context.Context, envsToCreate map[string]map[string]environment.Labels) {
+func (d *Db) createTenantEnvironments(ctx context.Context, envsToCreate map[string]environment.Labels) {
 	d.t.Helper()
+
 	for k, v := range envsToCreate {
-		tenant, err := d.repo.TenantCreate(ctx, &model.TenantCreate{
-			Name: k,
+		tenantName := strings.Split(k, ":")[0]
+		envName := strings.Split(k, ":")[1]
+		tenant, _ := d.repo.TenantCreate(ctx, &model.TenantCreate{
+			Name: tenantName,
 		})
-		if err != nil {
-			d.t.Fatalf("create tenant: %v", err)
-		}
-		for name, labels := range v {
-			d.createEnv(ctx, tenant, name, labels)
-		}
+		d.createEnv(ctx, tenant, envName, v)
 	}
 }
 
-func (d *Db) createDeployments(ctx context.Context, deployments []featureDeploy) {
-	for _, deploy := range deployments {
-		d.createDeployment(ctx, deploy.name, deploy.version, deploy.target)
+func startPostgresql(ctx context.Context, t *testing.T) (container *postgres.PostgresContainer, dsn string, err error) {
+	container, err = postgres.Run(
+		ctx,
+		"docker.io/postgres:16-alpine",
+		postgres.WithDatabase("test"),
+		postgres.WithUsername("test"),
+		postgres.WithPassword("test"),
+		postgres.WithSQLDriver("pgx"),
+		postgres.BasicWaitStrategies(),
+	)
+	defer testcontainers.CleanupContainer(t, container)
+
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to start container: %w", err)
 	}
+
+	dsn, err = container.ConnectionString(ctx, "sslmode=disable")
+	if err != nil {
+		return nil, "", fmt.Errorf("failed to get connection string: %w", err)
+	}
+
+	logger, _ := test.NewNullLogger()
+	if err = database.Migrate("pgx", dsn, logger); err != nil {
+		return nil, "", fmt.Errorf("failed to migrate database: %w", err)
+	}
+
+	if err = container.Snapshot(ctx); err != nil {
+		return nil, "", fmt.Errorf("failed to snapshot: %w", err)
+	}
+
+	return container, dsn, nil
 }
 
-func setupDb(ctx context.Context, t *testing.T, testcontainers bool) *Db {
-	t.Helper()
-
-	log := logrus.New()
-	log.Out = io.Discard
-
-	var pool *pgxpool.Pool
-	dbs := "postgres://postgres:postgres@127.0.0.1:5432/fasit?sslmode=disable"
-	if !testcontainers {
-		p, c, err := database.NewDB(ctx, dbs, false)
-		if err != nil {
-			t.Fatalf("Error connecting to database: %v", err)
-		}
-		pool = p
-		t.Cleanup(func() {
-			_ = c.Close()
-		})
-	} else {
-		d, c := dbtest.DockerSQLPool(ctx)
-		t.Logf("PostgreSQL DSN: %q", d)
-		dbs = d
-		db, closers, err := database.NewDB(ctx, dbs, false)
-		if err != nil {
-			log.Fatalf("Error connecting to database: %v", err)
-		}
-		t.Cleanup(func() {
-			_ = closers.Close()
-			c()
-		})
-		pool = db
+func getConnection(ctx context.Context, t *testing.T, container *postgres.PostgresContainer, dsn string, log logrus.FieldLogger) Db {
+	pool, _, err := database.NewDB(ctx, dsn, false)
+	if err != nil {
+		t.Fatalf("Error connecting to database: %v", err)
 	}
 
-	if err := database.Migrate("pgx", dbs, logrus.NewEntry(log)); err != nil {
-		t.Fatalf("Could not migrate: %v", err)
-	}
-	return &Db{
-		repo: database.New(pool, log.WithField("subsystem", "repo")),
+	t.Cleanup(func() {
+		pool.Close()
+		if err = container.Restore(ctx); err != nil {
+			t.Fatalf("failed to restore database: %v", err)
+		}
+	})
+	return Db{
+		repo: database.New(pool, log),
 		t:    t,
 		pool: pool,
 	}
