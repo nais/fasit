6b904e9bae89533dd58f9d97939642a99ed6ff65
Christer Edvartsen <christer.edvartsen@nav.no>
2026-01-06 16:26:52 +0100

    build: don't chain mise run tasks
    Co-authored-by: Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>

--- mise/tasks/ci/fmt.sh
+++ mise/tasks/ci/fmt.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 #MISE description="Ensure all code is formatted"
-#MISE depends=["generate"]
-
-mise run fmt
+#MISE depends=["generate", "fmt"]
 
 if ! git diff --exit-code --name-only; then
 	echo "The file(s) listed above are not formatted correctly. Please run \`mise run fmt\` and commit the changes."
--- mise/tasks/fmt.sh
+++ mise/tasks/fmt.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 #MISE description="Format all code"
 #MISE depends=["fmt:*"]
+#MISE wait_for=["generate"]
