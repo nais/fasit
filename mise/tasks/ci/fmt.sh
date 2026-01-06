ce3a31376e6b89c756ec40af1c3fce11171722fc
Christer Edvartsen <christer.edvartsen@nav.no>
2026-01-06 16:34:56 +0100

    build: show diff

--- mise/tasks/ci/fmt.sh
+++ mise/tasks/ci/fmt.sh
@@ -4,5 +4,6 @@
 
 if ! git diff --exit-code --name-only; then
 	echo "The file(s) listed above are not formatted correctly. Please run \`mise run fmt\` and commit the changes."
+	git diff
 	exit 1
 fi
