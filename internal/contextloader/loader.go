3391aa262f05ae4b6f6a6d1b777bddd9434ed769
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2026-05-07 12:59:46 +0200

    feat(dbtx): add transactional context helper

--- internal/contextloader/loader.go
+++ internal/contextloader/loader.go
@@ -6,6 +6,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/nais/fasit/internal/audit"
 	"github.com/nais/fasit/internal/cost"
+	"github.com/nais/fasit/internal/dbtx"
 	"github.com/nais/fasit/internal/deployment"
 	"github.com/nais/fasit/internal/environment"
 	"github.com/nais/fasit/internal/feature"
@@ -30,6 +31,7 @@ func NewLoaderFunc(
 	}
 
 	return func(ctx context.Context) context.Context {
+		ctx = dbtx.Register(ctx, pool)
 		ctx = audit.Register(ctx, pool, log)
 		ctx = cost.Register(ctx, pool)
 		ctx = environment.Register(ctx, pool)
