f75f2d0dc21fdd7705e5c6750c6ea59a014312f7
Christer Edvartsen <christer.edvartsen@nav.no>
2026-05-18 10:53:26 +0200

    refactor: inline naisd release script

--- mise/tasks/release/naisd.sh
+++ mise/tasks/release/naisd.sh
@@ -2,4 +2,18 @@
 #MISE description="Release naisd"
 set -euo pipefail
 
-./hack/release-naisd.sh
+# If there's changes locally (only tracked files), or the commits aren't pushed, exit with error
+if [ -n "$(git status --untracked-files=no --porcelain=v1)" ] || [ -n "$(git log origin/main..HEAD)" ]; then
+  echo "There are uncommitted changes or commits not pushed to origin/master"
+  exit 1
+fi
+
+tag="naisd-$(date +%Y%m%d%H%M%S)"
+# If there's no changes locally, tag the release with `naisd-[datetime]` and push it to origin
+git tag -a "$tag" -m "Release naisd"
+
+# Push the tag to origin
+git push origin --tags
+
+# Delete the tag locally
+git tag -d "$tag"
