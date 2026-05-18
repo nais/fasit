cfc1146b9f377d1a71f5b978e4d6c530d0988007
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2026-05-18 20:37:12 +0200

    fix: remove remainig mockery stuff

--- mise/tasks/generate/mocks.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#MISE description="Generate mocks"
-#MISE wait_for=["generate:sqlc"]
-#MISE depends_post=["fmt:go"]
-#MISE sources=["internal/**/*.go", ".mockery.yaml"]
-#MISE outputs=["internal/**/mocks/*.go"]
-set -euo pipefail
-
-go tool github.com/vektra/mockery/v3
