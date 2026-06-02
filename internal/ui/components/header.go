935de901255fca5f130d1a048b6e7ccfbef84929
Johnny Fredheim Horvi <johnny@horvi.no>
2026-06-02 14:36:47 +0200

    fix(ui): Activity Log -> Audit log

--- internal/ui/components/header.go
+++ internal/ui/components/header.go
@@ -51,7 +51,7 @@ func SiteHeader(currentPage Page, userEmail string, hideSearch bool, appVersion 
 			),
 			h.Div(h.Class("kebab-menu"), h.ID("nav-kebab"),
 				h.A(h.Href("/assignments"), h.Class("kebab-item"), g.Text("Assignments")),
-				h.A(h.Href("/auditlog"), h.Class("kebab-item"), g.Text("Activity Log")),
+				h.A(h.Href("/auditlog"), h.Class("kebab-item"), g.Text("Audit log")),
 				h.A(h.Href("/labels"), h.Class("kebab-item"), g.Text("Labels")),
 				h.A(h.Href("/naisd"), h.Class("kebab-item"), g.Text("Naisd")),
 				h.A(h.Href("/reconciler"), h.Class("kebab-item"), g.Text("Reconciler")),
