3265a7838927eb5b0dc9c6ae0b1c63775db24f3b
Tommy Trøen <tommy.troen@nav.no>
2026-06-05 12:40:22 +0200

    refactor: fmt

--- internal/database/migrations/0092_deploy_instructions_latest_covering_index.sql
+++ internal/database/migrations/0092_deploy_instructions_latest_covering_index.sql
@@ -1,6 +1,5 @@
 -- +goose Up
 DROP INDEX IF EXISTS deploy_instructions_latest_idx;
 
-CREATE INDEX IF NOT EXISTS deploy_instructions_latest_idx
-	ON deploy_instructions(feature_name, environment_id, created DESC)
-	INCLUDE (id, hash, status, feature_assignment_id);
+CREATE INDEX IF NOT EXISTS deploy_instructions_latest_idx ON deploy_instructions(feature_name, environment_id, created DESC) INCLUDE (id, hash, status, feature_assignment_id);
+
