aca38a0dbefb65269e67cd709d0dbc33f7775511
Christer Edvartsen <christer.edvartsen@nav.no>
2026-06-09 15:36:33 +0200

    build: remove sources

--- mise/tasks/generate/feature-schema.sh
+++ mise/tasks/generate/feature-schema.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 #MISE description="Generate feature schema"
 #MISE depends_post=["fmt:json"]
-#MISE sources=["cmd/generate_schema/main.go"]
-#MISE outputs=["schema/jsonschema/feature.json"]
 set -euo pipefail
 
 go run cmd/generate_schema/main.go
