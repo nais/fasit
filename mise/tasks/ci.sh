2ca974feac4394d62c8056d5a09bffece8dab154
Johnny Fredheim Horvi <johnny@horvi.no>
2026-06-09 15:30:27 +0200

    mise: rename and modify precommit to run the same as ci

---
+++ mise/tasks/ci.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+#MISE description="Run what CI would run"
+#MISE depends=["generate", "fmt", "check", "test"]
--- mise/tasks/precommit.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-#MISE description="Generate, format, and check (pre-commit gate)"
-#MISE depends=["generate", "fmt", "check"]
